f8dc11acd4f2ffd9ca518dbf7038e7dbf5fbd1c7
koushik <koushik.koushik.kumar@gmail.com>
2020-09-06 13:18:42 +0530

    Ready for pushing to git

--- src/firebase.js
+++ src/firebase.js
@@ -7,8 +7,8 @@ const config = {
   projectId: "<your-cloud-firestore-project>",
   storageBucket: "<your-storage-bucket>",
   messagingSenderId: "<your-sender-id>",
-  appId: "1:1096152597569:web:6deff0f873f780273ff2c0",
-  measurementId: "G-27C31YBJ7G",
+  appId: "<your-app-id>",
+  measurementId: "<your id>",
 };
 
 const firebaseApp = firebase.initializeApp(config);
