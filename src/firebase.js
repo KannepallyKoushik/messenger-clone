d4ab7d016580e2b965f2fd14657c4882eac818ff
koushik <koushik.koushik.kumar@gmail.com>
2020-09-06 13:13:36 +0530

    removed firebase db creds

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyDe8XKc21JYoG8xOhwOfOyyKuxARNz33hg",
-  authDomain: "messenger-clone-fb.firebaseapp.com",
-  databaseURL: "https://messenger-clone-fb.firebaseio.com",
-  projectId: "messenger-clone-fb",
-  storageBucket: "messenger-clone-fb.appspot.com",
-  messagingSenderId: "1096152597569",
+  apiKey: "<Your own api key>",
+  authDomain: "<your-auth-domain>",
+  databaseURL: "<your-database-url>",
+  projectId: "<your-cloud-firestore-project>",
+  storageBucket: "<your-storage-bucket>",
+  messagingSenderId: "<your-sender-id>",
   appId: "1:1096152597569:web:6deff0f873f780273ff2c0",
   measurementId: "G-27C31YBJ7G",
 };
